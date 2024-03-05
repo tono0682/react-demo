02cf6a1bcb727ae43bea782af7b23568d921aa2a
Tyla <tyla.ono@gmail.com>
2024-03-05 19:09:34 +1000

    Add hero component and styling

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+
+export default function Hero () {
+    
+    return (
+        <header className='hero'>
+        <div className='hero-content'>
+          <h1>Welcome to G Automotive</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <button className='btn'>Contact Us</button>
+        </div>
+      </header>
+    )
+}
