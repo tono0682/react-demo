f661c562702677d3e4a10a572ea63d1f3b4db4c5
Tyla <tyla.ono@gmail.com>
2024-03-05 19:13:47 +1000

    Add components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './components/Hero'
-
+import Cards from './components/Cards'
+import { cards } from './data/cards'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className='page'>
       <Hero />
       <main className='grow'>
+          <Cards cards={cards} />
       </main>
     </div>
   )
