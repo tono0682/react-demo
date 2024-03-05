1f5f1117418c072554b9a5eed5ed17c2e38b6480
Tyla <tyla.ono@gmail.com>
2024-03-05 19:11:27 +1000

    Add cards mock data

---
+++ src/data/cards.ts
@@ -0,0 +1,20 @@
+export const cards = [
+    {
+      id: 1,
+      header: "Heading 1",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+      image: "/images/brisbane.jpg",
+    },
+    {
+      id: 2,
+      header: "Heading 2",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+      image: "/images/brisbane.jpg",
+    },
+    {
+      id: 3,
+      header: "Heading 3",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna\n\nincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris",
+      image: "/images/brisbane.jpg",
+    },
+  ]
